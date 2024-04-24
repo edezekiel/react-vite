f1c825e1e037e3bc65430fab224173a07c17b0cf
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2024-04-24 11:07:33 -0500

    fix(): Fix test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -6,6 +6,6 @@ import App from './App';
 describe('<App />', () => {
   test('App mounts properly', () => {
     const wrapper = render(<App />)
-    expect(wrapper).not.toBeTruthy()
+    expect(wrapper).toBeTruthy()
   });
 });
